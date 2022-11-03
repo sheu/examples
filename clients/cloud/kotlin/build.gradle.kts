5531361cceabff9a5f070f52fd0ad5ddf8b442e3
Sheunesu Gumbie <sheunesu.gumbi@partner.bmw.co.za>
2022-11-03 10:48:10 +0200

    Upgrade kotlin version

--- clients/cloud/kotlin/build.gradle.kts
+++ clients/cloud/kotlin/build.gradle.kts
@@ -1,47 +1,56 @@
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "io.confluent.confluent"
 
 plugins {
+  kotlin("jvm") version "1.7.10"
+  application
   java
-  kotlin("jvm") version "1.3.11"
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
-}
+
 
 dependencies {
-  compile(kotlin("stdlib"))
-  compile(kotlin("reflect"))
+  implementation(kotlin("stdlib"))
+  implementation(kotlin("reflect"))
 
-  compile("org.apache.kafka:kafka-clients:2.1.0")
-  compile("org.apache.kafka:kafka-streams:2.1.0")
-  compile("org.apache.kafka:connect-runtime:2.1.0")
-  compile("io.confluent:kafka-json-serializer:5.0.1")
-  compile("org.slf4j:slf4j-api:1.7.6")
-  compile("org.slf4j:slf4j-log4j12:1.7.6")
-  compile("com.fasterxml.jackson.core:jackson-databind:[2.8.11.1,)")
+  implementation("org.apache.kafka:kafka-clients:3.3.1")
+  implementation("org.apache.kafka:kafka-streams:3.3.1")
+  implementation("org.apache.kafka:connect-runtime:3.3.1")
+  implementation("io.confluent:kafka-json-serializer:5.0.1")
+  implementation("org.slf4j:slf4j-api:2.0.3")
+  implementation("org.slf4j:slf4j-log4j12:2.0.3")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
   //compile ("com.fasterxml.jackson.module:jackson-module-kotlin:[2.8.11.1,)")
-  compile("com.google.code.gson:gson:2.2.4")
+  implementation("com.google.code.gson:gson:2.9.0")
+  implementation("io.confluent:kafka-json-schema-serializer:5.5.1")
+  implementation("com.github.everit-org.json-schema:org.everit.json.schema:1.12.1")
 }
 
 repositories {
+  maven(url = "https://packages.confluent.io/maven/")
+  maven(
+    url = "https://jitpack.io"
+  )
+  mavenCentral()
   jcenter()
-  maven(url = "http://packages.confluent.io/maven/")
 }
 
 val configPath: String by project
 val topic: String by project
-val mainClass: String by project
+val projectMainClassName: String by project
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.jvmTarget = "11"
 }
 
-task("runApp", JavaExec::class) {
-  classpath = sourceSets["main"].runtimeClasspath
-  main = mainClass
-  args = listOf(configPath, topic)
+application {
+  this.mainClass.set(projectMainClassName)
+
+}
+tasks.register<JavaExec>("runApp") {
+  args = listOf( configPath, topic)
+  mainClass.set(projectMainClassName)
+  classpath =  sourceSets["main"].runtimeClasspath
 }
